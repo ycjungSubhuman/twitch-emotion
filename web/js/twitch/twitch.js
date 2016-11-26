1c5d47a9147e37ca61cd6cab8db478fde07ec2fd
ycjung <ycjung@postech.ac.kr>
2016-11-26 17:05:39 +0900

    Change Auth Callback

--- web/js/twitch/twitch.js
+++ web/js/twitch/twitch.js
@@ -1,11 +1,9 @@
 const T = {
-		init: function() {
+	init: function() {
 		Twitch.init({clientId: 'a6he265cackgwqtc89gzqfivu5yn3n6'}, function(error, status) {
 			if(error) console.log(error);
-			if(!status.authenticated) {
-				_request_auth();
-			}
 		});
+		_request_auth();
 	},
 };
 
