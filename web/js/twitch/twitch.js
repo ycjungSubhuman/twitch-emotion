6ab4170d72f5f8abe37d6d0ae4e4b32f54425536
ycjung <ycjung@postech.ac.kr>
2016-11-26 17:43:51 +0900

    Add Video

--- web/js/twitch/twitch.js
+++ web/js/twitch/twitch.js
@@ -2,6 +2,10 @@ const T = {
 	init: function() {
 		Twitch.init({clientId: 'a6he265cackgwqtc89gzqfivu5yn3n6'}, function(error, status) {
 			if(error) console.log(error);
+			if(status.authenticated) {
+				$('#login').hide();
+				get_video("subhuman22");
+			}
 		});
 		$('#login').click(function() {
 			_request_auth();
@@ -13,4 +17,14 @@ function _request_auth() {
 	Twitch.login({
 		scope: ['user_read', 'channel_read'],
 	});
+}
+
+function get_video(channel) {
+	var options = {
+		width: 854,
+		height: 480,
+		channel: channel,
+	};
+	var player = new Twitch.Player("video", options);
+	player.setVolume(0.5);
 }
