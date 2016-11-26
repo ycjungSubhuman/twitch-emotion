1f9baab68713fb8f5060d6693f5a1421937a343f
ycjung <ycjung@postech.ac.kr>
2016-11-26 17:12:57 +0900

    Add Logging

--- web/js/twitch/twitch.js
+++ web/js/twitch/twitch.js
@@ -3,9 +3,10 @@ const T = {
 		Twitch.init({clientId: 'a6he265cackgwqtc89gzqfivu5yn3n6'}, function(error, status) {
 			if(error) console.log(error);
 		});
-		
+
 		Twitch.getStatus(function(error, status) {
 			if(!status.authenticated) {
+				console.log(status.authenticated);
 				_request_auth();
 			}
 		});
