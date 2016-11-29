23c79108ba7b1dc450d2220c2b1898ee93437ad1
ycjung <ycjung@postech.ac.kr>
2016-11-29 17:33:15 +0900

    Send Image To MS

--- web/js/device/cam.js
+++ web/js/device/cam.js
@@ -6,7 +6,41 @@ const Cam = {
 				var img = document.createElement('img');
 				img.setAttribute("src", data_uri);
 				document.querySelector('#test_capture').appendChild(img);
+
+				var data = dataURItoBlob(data_uri);
+				$.ajax({
+					type: 'POST',
+					headers: {
+						'Content-Type': 'blob',
+						'Ocp-Apim-Subscription-Key': '39d48e5981694d35aba599b8a653c5f6',
+					},
+					url: 'https://api.projectoxford.ai/emotion/v1.0/recognize',
+					content_type: data,
+					success: function(result) {
+						console.log(result);
+					}
+				});
 			});
 		});
 	}
+}
+
+function dataURItoBlob(dataURI) {
+    // convert base64/URLEncoded data component to raw binary data held in a string
+    var byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+        byteString = atob(dataURI.split(',')[1]);
+    else
+        byteString = unescape(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to a typed array
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ia], {type:mimeString});
 }
