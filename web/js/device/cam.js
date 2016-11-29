c1f8d3adb7308d4fafdd096bed05b07a1f0cad13
Ubuntu <ubuntu@ip-172-31-27-215.us-west-2.compute.internal>
2016-11-29 09:31:19 +0000

    Image Sent To MS And Recieves Data Back

--- web/js/device/cam.js
+++ web/js/device/cam.js
@@ -3,7 +3,6 @@ const Cam = {
 		Webcam.attach('#camera');	
 		$('#snapshot').click(function() {
 			Webcam.snap(function(data_uri) {
-				console.log(data_uri);
 				var img = document.createElement('img');
 				img.setAttribute("src", data_uri);
 				document.querySelector('#test_capture').appendChild(img);
@@ -19,12 +18,9 @@ const Cam = {
 					url: 'https://api.projectoxford.ai/emotion/v1.0/recognize',
 					data: data,
 					success: function(result) {
-						var obj = eval(result.responseText);
-						var emotion = obj.scores;
-						updateEmotion(emotion);
+						updateEmotion(result[0].scores);
 					}
 				});
-				console.log(a);
 			});
 		});
 	}
