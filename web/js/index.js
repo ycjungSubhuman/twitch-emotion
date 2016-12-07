93dff9c9a8bbef15da946f4437e378ef4f9a12d2
Ubuntu <ubuntu@ip-172-31-27-215.us-west-2.compute.internal>
2016-12-07 07:10:19 +0000

    Add Channel Search Input

--- web/js/index.js
+++ web/js/index.js
@@ -4,4 +4,9 @@ window.onload = function() {
   graph_init();
   smiley_init();
   init_fetch();
+  $('#channel').keydown(function(e) {
+    if(e.keyCode==13) {
+      T.get($('#channel').val());
+    }
+  });
 };
